3575008508a5c05766057c0a1d0620cf2a93ca00
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-11-19 03:13:28 +0700

    feat: init project

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
